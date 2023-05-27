7de59f1fb9b4a8cc79c4cd18da893c0cc6b0337a
Frognar <sebastian.przyszlak@gmail.com>
2023-05-27 10:24:11 +0200

    Create SequenceNode implementation

---
+++ IntelliBranch/Assets/IntelliBranch/Scripts/SequenceNode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Frognar.IntelliBranch {
+  public class SequenceNode : Node {
+    readonly IEnumerable<Node> nodes;
+
+    public SequenceNode(IEnumerable<Node> nodes) {
+      this.nodes = nodes;
+    }
+
+    public override bool Execute() {
+      return nodes.All(node => node.Execute());
+    }
+  }
+}
