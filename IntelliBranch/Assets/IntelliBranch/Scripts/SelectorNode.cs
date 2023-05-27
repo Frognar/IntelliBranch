3f5b96415d471e2a0026da21e38eb831f5be355b
Frognar <sebastian.przyszlak@gmail.com>
2023-05-27 10:25:37 +0200

    Create SelectorNode implementation

---
+++ IntelliBranch/Assets/IntelliBranch/Scripts/SelectorNode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Frognar.IntelliBranch {
+  public class SelectorNode : Node {
+    readonly IEnumerable<Node> nodes;
+
+    public SelectorNode(IEnumerable<Node> nodes) {
+      this.nodes = nodes;
+    }
+
+    public override bool Execute() {
+      return nodes.Any(node => node.Execute());
+    }
+  }
+}
