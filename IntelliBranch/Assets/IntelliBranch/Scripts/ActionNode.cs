854ea2f7b9b7be4b50ad3b7f43675be9629eda22
Frognar <sebastian.przyszlak@gmail.com>
2023-05-27 10:06:35 +0200

    Create ActionNode implementation

---
+++ IntelliBranch/Assets/IntelliBranch/Scripts/ActionNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Frognar.IntelliBranch {
+  public class ActionNode : Node {
+    readonly Func<bool> action;
+
+    public ActionNode(Func<bool> action) {
+      this.action = action;
+    }
+
+    public override bool Execute() {
+      return action();
+    }
+  }
+}
