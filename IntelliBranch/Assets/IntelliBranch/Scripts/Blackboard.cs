c24641db4d4ebd184e4805e45e37408cc73f0917
Frognar <sebastian.przyszlak@gmail.com>
2023-05-28 08:26:42 +0200

    Create Blackboard class

---
+++ IntelliBranch/Assets/IntelliBranch/Scripts/Blackboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Frognar.Results.Scripts;
+
+namespace Frognar.IntelliBranch {
+  public class Blackboard {
+    readonly Dictionary<string, object> data = new();
+
+    public Result<T> Get<T>(string key) {
+      if (data.TryGetValue(key, out object value) == false) {
+        return new KeyNotFoundException($"No data found for key '{key}'.");
+      }
+
+      if (value is T tValue) {
+        return tValue;
+      }
+
+      return new InvalidCastException($"Value of type {value.GetType()} cannot be cast to {typeof(T)}");
+    }
+
+    public void Set(string key, object value) {
+      data[key] = value;
+    }
+  }
+}
