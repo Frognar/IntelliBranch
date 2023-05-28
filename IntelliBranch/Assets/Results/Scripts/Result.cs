ac8b5072f2c26df120f0e920441b69d20084d1df
Frognar <sebastian.przyszlak@gmail.com>
2023-05-28 08:26:27 +0200

    Create Result struct

---
+++ IntelliBranch/Assets/Results/Scripts/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Frognar.Results.Scripts {
+  public readonly struct Result<T> {
+    readonly T value;
+    readonly Exception error;
+    bool IsSuccessful { get; }
+    public T Value => IsSuccessful ? value : throw error;
+    
+    public Result(T value) {
+      this.value = value;
+      error = default;
+      IsSuccessful = true;
+    }
+
+    public Result(Exception error) {
+      value = default;
+      this.error = error;
+      IsSuccessful = false;
+    }
+
+    public static implicit operator Result<T>(T result) => new(result);
+    public static implicit operator Result<T>(Exception error) => new(error);
+    public static bool operator true(in Result<T> result) => result.IsSuccessful;
+    public static bool operator false(in Result<T> result) => result.IsSuccessful == false;
+  }
+}
