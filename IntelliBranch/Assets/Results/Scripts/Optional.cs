cf82588afa8f8f9bcab0902b5414d8e90dd00531
Frognar <sebastian.przyszlak@gmail.com>
2023-05-28 20:09:04 +0200

    Don't throw exceptions using Value getter and initialize static None value once

--- IntelliBranch/Assets/Results/Scripts/Optional.cs
+++ IntelliBranch/Assets/Results/Scripts/Optional.cs
@@ -1,18 +1,20 @@
-﻿namespace Frognar.Results {
+﻿using System;
+
+namespace Frognar.Results {
   public readonly struct Optional<T> {
     readonly T value;
-    public T Value => HasValue ? value : throw new System.InvalidOperationException("Optional has no value.");
+    public Result<T> Value => HasValue ? value : new InvalidOperationException("Optional has no value.");
     public bool HasValue { get; }
-    
+
     public Optional(T value) : this(value, true) {
     }
-    
+
     Optional(T value, bool hasValue) {
       this.value = value;
       HasValue = hasValue;
     }
 
-    public static Optional<T> None => new(default, false);
+    public static Optional<T> None { get; } = new(default, false);
     public static implicit operator Optional<T>(T result) => new(result);
     public static bool operator true(in Optional<T> result) => result.HasValue;
     public static bool operator false(in Optional<T> result) => result.HasValue == false;
